76e69b03a2c4c663e17a9e3d62c9a70ea45ddb51
Denis Konovalov <den.konovaloff@gmail.com>
2018-10-19 14:46:15 +0400

    Создал проект

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Konovalov Denis
+namespace HardSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
